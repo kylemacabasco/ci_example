a57d90b90f1158fdf4a5c0271c597f6e392814bf
kylemacabasco <kylemacabasco@gmail.com>
2022-11-10 13:29:46 -0800

    taking out one of the test

--- tests.py
+++ tests.py
@@ -7,10 +7,7 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-
-    def test2(self):
-        expected = "Goodbye World"
-        self.assertEqual(task.my_func(), expected)
+        
 
 if __name__ == '__main__':
     unittest.main()
